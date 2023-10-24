658110280ddb3fa1e5347192040ce22f21c050ad
Rohit Manohar <122254093+rohitmanohar2108@users.noreply.github.com>
2023-10-24 23:03:29 +0530

    Update chess_clock.v

--- chess_clock.v
+++ chess_clock.v
@@ -1,88 +1,92 @@
-module chess_clock (
-    input wire clk,
-    input wire reset,
-    input wire start,
-    input wire pause,
-    input wire player1_add_time,
-    input wire player2_add_time,
-    output wire [15:0] player1_time,
-    output wire [15:0] player2_time,
-    output wire player1_flag,
-    output wire player2_flag
+module chess_clock(
+  input wire clk,             // Clock input
+  input wire reset,           // Reset signal for timers
+  input wire start,           // Start signal for timers
+  input wire surrender_player1, // Surrender button for player 1
+  input wire surrender_player2, // Surrender button for player 2
+  input wire switch_turn,     // Switch turn signal
+  output reg [6:0] seg_player1_min1, // 7-segment display for player 1 minutes (tens)
+  output reg [6:0] seg_player1_min0, // 7-segment display for player 1 minutes (units)
+  output reg [6:0] seg_player1_sec1, // 7-segment display for player 1 seconds (tens)
+  output reg [6:0] seg_player1_sec0, // 7-segment display for player 1 seconds (units)
+  output reg [6:0] seg_player2_min1, // 7-segment display for player 2 minutes (tens)
+  output reg [6:0] seg_player2_min0, // 7-segment display for player 2 minutes (units)
+  output reg [6:0] seg_player2_sec1, // 7-segment display for player 2 seconds (tens)
+  output reg [6:0] seg_player2_sec0, // 7-segment display for player 2 seconds (units)
+  output reg player1_green_led,     // Green LED indicating player 1's victory
+  output reg player2_green_led,     // Green LED indicating player 2's victory
+  output reg player1_red_led,       // Red LED indicating player 1's loss
+  output reg player2_red_led        // Red LED indicating player 2's loss
 );
 
-reg [15:0] player1_time;
-reg [15:0] player2_time;
-reg player1_flag;
-reg player2_flag;
-reg [1:0] state;
-reg [15:0] player1_delay;
-reg [15:0] player2_delay;
+  reg [3:0] min_ones_player1; // 4-bit register for player 1 minutes (units)
+  reg [3:0] min_tens_player1; // 4-bit register for player 1 minutes (tens)
+  reg [3:0] sec_ones_player1; // 4-bit register for player 1 seconds (units)
+  reg [3:0] sec_tens_player1; // 4-bit register for player 1 seconds (tens)
 
-parameter COUNT_MAX = 16'd50000; // Adjust this value for different clock speeds
-parameter DELAY_MAX = 16'd1000; // Maximum delay time
+  reg [3:0] min_ones_player2; // 4-bit register for player 2 minutes (units)
+  reg [3:0] min_tens_player2; // 4-bit register for player 2 minutes (tens)
+  reg [3:0] sec_ones_player2; // 4-bit register for player 2 seconds (units)
+  reg [3:0] sec_tens_player2; // 4-bit register for player 2 seconds (tens)
 
-always @(posedge clk or posedge reset) begin
+  reg [1:0] player_turn; // 2-bit counter to track player turns (00: Player 1, 01: Player 2)
+
+  always @(posedge clk or posedge reset) begin
     if (reset) begin
-        player1_time <= 16'b0000_0000_0000_0000;
-        player2_time <= 16'b0000_0000_0000_0000;
-        player1_flag <= 1'b1;
-        player2_flag <= 1'b0;
-        state <= 2'b00;
-    end else if (start) begin
-        case (state)
-            2'b00: begin // Player 1's turn
-                if (player1_time < COUNT_MAX) begin
-                    player1_time <= player1_time + 1;
-                end else begin
-                    player1_time <= 16'b0000_0000_0000_0000;
-                    player1_flag <= 1'b0;
-                    state <= 2'b01;
-                end
-            end
-            2'b01: begin // Player 2's turn
-                if (player2_time < COUNT_MAX) begin
-                    player2_time <= player2_time + 1;
-                end else begin
-                    player2_time <= 16'b0000_0000_0000_0000;
-                    player2_flag <= 1'b0;
-                    state <= 2'b00;
-                end
-            end
-        endcase
-    end
-end
+      min_ones_player1 <= 4'd0;
+      min_tens_player1 <= 4'd0;
+      sec_ones_player1 <= 4'd0;
+      sec_tens_player1 <= 4'd0;
 
-always @(posedge clk) begin
-    if (start && !pause) begin
-        if (player1_flag) begin
-            player1_time <= player1_time - 1;
-        end else begin
-            player2_time <= player2_time - 1;
+      min_ones_player2 <= 4'd0;
+      min_tens_player2 <= 4'd0;
+      sec_ones_player2 <= 4'd0;
+      sec_tens_player2 <= 4'd0;
+
+      player_turn <= 2'b00;
+
+      seg_player1_min1 <= 7'b0000000;   // Initialize 7-segment displays to 0
+      seg_player1_min0 <= 7'b0000000;
+      seg_player1_sec1 <= 7'b0000000;
+      seg_player1_sec0 <= 7'b0000000;
+
+      seg_player2_min1 <= 7'b0000000;
+      seg_player2_min0 <= 7'b0000000;
+      seg_player2_sec1 <= 7'b0000000;
+      seg_player2_sec0 <= 7'b0000000;
+
+      player1_green_led <= 1'b0;
+      player2_green_led <= 1'b0;
+      player1_red_led <= 1'b0;
+      player2_red_led <= 1'b0;
+    end
+    else if (start) begin
+      // Timer logic for player 1
+      if (player_turn == 2'b00) begin
+        if (sec_ones_player1 < 4'd9) begin
+          sec_ones_player1 <= sec_ones_player1 + 1;
         end
+        else if (sec_tens_player1 < 4'd5) begin
+          sec_ones_player1 <= 4'd0;
+          sec_tens_player1 <= sec_tens_player1 + 1;
+        end
+        else if (min_ones_player1 < 4'd9) begin
+          sec_tens_player1 <= 4'd0;
+          sec_ones_player1 <= 4'd0;
+          min_ones_player1 <= min_ones_player1 + 1;
+        end
+        else if (min_tens_player1 < 4'd5) begin
+          sec_tens_player1 <= 4'd0;
+          sec_ones_player1 <= 4'd0;
+          min_ones_player1 <= 4'd0;
+          min_tens_player1 <= min_tens_player1 + 1;
+        end
+        else begin
+          // Player 1 has won, set LEDs accordingly
+          player1_green_led <= 1'b1;
+          player2_red_led <= 1'b1;
+        end
+      end
     end
-end
-
-always @(posedge clk) begin
-    if (player1_add_time && player1_time + player1_delay < COUNT_MAX) begin
-        player1_delay <= player1_delay + 1;
-    end else if (!player1_add_time) begin
-        player1_delay <= 16'b0000_0000_0000_0000;
-    end
-    if (player2_add_time && player2_time + player2_delay < COUNT_MAX) begin
-        player2_delay <= player2_delay + 1;
-    end else if (!player2_add_time) begin
-        player2_delay <= 16'b0000_0000_0000_0000;
-    end
-end
-
-always @(posedge clk) begin
-    if (start) begin
-        case (state)
-            2'b00: player1_flag <= 1'b1;
-            2'b01: player2_flag <= 1'b1;
-        endcase
-    end
-end
-
+  end
 endmodule
